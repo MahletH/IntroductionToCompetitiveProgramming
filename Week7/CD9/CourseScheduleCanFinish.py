9c28174f99874a484295b6ed2d452896cb505abd
MahletH <47258462+MahletH@users.noreply.github.com>
2020-02-12 15:53:32 +0300

    Add CourseScheduleCanFinish

---
+++ Week7/CD9/CourseScheduleCanFinish.py
@@ -0,0 +1,25 @@
+import collections
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre=[0]*numCourses
+        for lst in prerequisites:
+            pre[lst[0]]+=1
+        que=collections.deque()
+        res=[]
+        visited=set()
+        for i in range(numCourses):
+            if not pre[i]:
+                que.append(i)
+                visited.add(i)
+        while(len(que)):
+            curr=que.popleft()
+            res.append(curr)
+            for lst in prerequisites:
+                if lst[1]==curr:
+                    pre[lst[0]]-=1
+                if not pre[lst[0]] and lst[0] not in visited:
+                    que.append(lst[0])
+                    visited.add(lst[0])                    
+        if len(res)==numCourses:
+            return True
+        return False
